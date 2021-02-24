5f619a4f1c0c08515c8903a1988f791b1938f07d
Daisuke <mds87@hotmail.com>
2021-02-24 19:58:52 +0100

    finished firebase react auth tutorial

--- src/base.js
+++ src/base.js
@@ -1,14 +1,14 @@
-import * as firebase from "firebase/app";
+import firebase from 'firebase/app'
 import "firebase/auth";
 
 const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  })
+  apiKey: "AIzaSyDXtCJEHHZIG9rCgkoNc6nLRruUjUZmpZo",
+  authDomain: "fir-react-auth-57b44.firebaseapp.com",
+  projectId: "fir-react-auth-57b44",
+  storageBucket: "fir-react-auth-57b44.appspot.com",
+  messagingSenderId: "609711529785",
+  appId: "1:609711529785:web:c330b935e5bce328bf52e8",
+  measurementId: "G-WDR9XS1YTF"
+})
 
   export default app
